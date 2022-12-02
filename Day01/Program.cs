7f9ffdfe0c248a138555a83c5db2d1dab4e885c0
Natalya Shtikel <shtikel.n@gmail.com>
2022-12-02 19:09:47 +0600

    Day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,35 @@
+﻿var lines = File.ReadLines("input.txt");
+
+var maxCallories = lines
+    .CalculateCallories()
+    .Max();
+
+Console.WriteLine(maxCallories);
+
+// PART 2
+var top3Sum = lines
+    .CalculateCallories()
+    .OrderDescending()
+    .Take(3)
+    .Sum();
+
+Console.WriteLine(top3Sum);
+
+static class IEnumerableExtensions
+{
+    public static IEnumerable<int> CalculateCallories(this IEnumerable<string> lines)
+    {
+        var currentElfCallories = 0;
+        foreach (var line in lines)
+        {
+            if (int.TryParse(line, out int currentCallory))
+            {
+                currentElfCallories += currentCallory;
+                continue;
+            }
+            int result = currentElfCallories;
+            currentElfCallories = 0;
+            yield return result;
+        }
+    }
+}
